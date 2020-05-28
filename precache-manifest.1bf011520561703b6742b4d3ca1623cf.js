f9bc8713c6f4c3cb9cbf082eb3c30d3f9299934d
sergeKash <39168159+sergeKashkin@users.noreply.github.com>
2020-05-28 10:55:14 +0300

    Updates

---
+++ precache-manifest.1bf011520561703b6742b4d3ca1623cf.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba99891537fd074d576ff219fa04042",
+    "url": "/daily_scope/index.html"
+  },
+  {
+    "revision": "8d7257644948fe296306",
+    "url": "/daily_scope/static/css/main.8cc09121.chunk.css"
+  },
+  {
+    "revision": "0e395dd1565b8684408f",
+    "url": "/daily_scope/static/js/2.19279b2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daily_scope/static/js/2.19279b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7257644948fe296306",
+    "url": "/daily_scope/static/js/main.ae30db7e.chunk.js"
+  },
+  {
+    "revision": "241300a599b38fac87b9",
+    "url": "/daily_scope/static/js/runtime-main.aa42d766.js"
+  },
+  {
+    "revision": "8bf1576e743502fcabaf05b96b3fd1ed",
+    "url": "/daily_scope/static/media/aquarius-stars.8bf1576e.png"
+  },
+  {
+    "revision": "2d55417b3f6dde3367f6f96923578629",
+    "url": "/daily_scope/static/media/aquarius.2d55417b.png"
+  },
+  {
+    "revision": "df303ba1b99cbf7c765c24cf74b85f7c",
+    "url": "/daily_scope/static/media/aries-stars.df303ba1.png"
+  },
+  {
+    "revision": "e678008aa0f5e3ce326494c026418b33",
+    "url": "/daily_scope/static/media/aries.e678008a.png"
+  },
+  {
+    "revision": "649eb5183fa6eb88cc10076d179a1c36",
+    "url": "/daily_scope/static/media/cancer-stars.649eb518.png"
+  },
+  {
+    "revision": "87e2202e921248f04b1cf005a6d9cbb9",
+    "url": "/daily_scope/static/media/cancer.87e2202e.png"
+  },
+  {
+    "revision": "eaca6b36a6b675e65979ba7dc769f6ae",
+    "url": "/daily_scope/static/media/capricorn-stars.eaca6b36.png"
+  },
+  {
+    "revision": "f25b19eba4ad74dc666874e705b8b59e",
+    "url": "/daily_scope/static/media/capricorn.f25b19eb.png"
+  },
+  {
+    "revision": "8281d432e738aba3f1fc94ee3467f7c1",
+    "url": "/daily_scope/static/media/gemini-stars.8281d432.png"
+  },
+  {
+    "revision": "5eff4fe2f2a11233d86bc4eaf079d9a7",
+    "url": "/daily_scope/static/media/gemini.5eff4fe2.png"
+  },
+  {
+    "revision": "6dfaefd6da3c34205a67a36d53371959",
+    "url": "/daily_scope/static/media/leo-stars.6dfaefd6.png"
+  },
+  {
+    "revision": "d4174cb507b815713d7723e54dacb941",
+    "url": "/daily_scope/static/media/leo.d4174cb5.png"
+  },
+  {
+    "revision": "92b21043a4b564ba9227501c3312ad4c",
+    "url": "/daily_scope/static/media/libra-stars.92b21043.png"
+  },
+  {
+    "revision": "a4bd9e0289acb36876d7767a9bc92581",
+    "url": "/daily_scope/static/media/libra.a4bd9e02.png"
+  },
+  {
+    "revision": "dcd0df170d2de1559ebbcec34f2fe79a",
+    "url": "/daily_scope/static/media/pisces-stars.dcd0df17.png"
+  },
+  {
+    "revision": "77736bdcee6ae5a0e0b2fefd14cea568",
+    "url": "/daily_scope/static/media/pisces.77736bdc.png"
+  },
+  {
+    "revision": "b3a2c659a17de230c540b9b62a23103f",
+    "url": "/daily_scope/static/media/sagittarius-stars.b3a2c659.png"
+  },
+  {
+    "revision": "0d0d6a9753f7dfc85f6a9a3e87121e85",
+    "url": "/daily_scope/static/media/sagittarius.0d0d6a97.png"
+  },
+  {
+    "revision": "f2fac247f60acf17abe48d6ae4821f87",
+    "url": "/daily_scope/static/media/scorpio-stars.f2fac247.png"
+  },
+  {
+    "revision": "376612703ed29f5cc9e59775e4dc1447",
+    "url": "/daily_scope/static/media/scorpio.37661270.png"
+  },
+  {
+    "revision": "05794f06aa8621705b012b963d637d59",
+    "url": "/daily_scope/static/media/sky.05794f06.png"
+  },
+  {
+    "revision": "c08b68eef3719881b67ed99130a1b714",
+    "url": "/daily_scope/static/media/taurus-stars.c08b68ee.png"
+  },
+  {
+    "revision": "6f576a03ae21886504b4c3ea6e5e626a",
+    "url": "/daily_scope/static/media/taurus.6f576a03.png"
+  },
+  {
+    "revision": "25abe70c54c55a89ace85b2e8d651966",
+    "url": "/daily_scope/static/media/virgo-stars.25abe70c.png"
+  },
+  {
+    "revision": "fdee23e4bcc7f4be9e48931a122262f5",
+    "url": "/daily_scope/static/media/virgo.fdee23e4.png"
+  }
+]);
